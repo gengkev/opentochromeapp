04ff98ee9826cf9d51f59877c17b82fd7834fa45
gengkev@gmail.com <gengkev@gmail.com@127a0e5a-feb4-0d66-0e51-42b92635bc4d>
2011-04-03 17:33:26 +0000

    updated converter to google doc

--- trunk/converter.js
+++ trunk/converter.js
@@ -1,4 +1,4 @@
-function convertWebApp(string) {
+function convertWebApp(string,origin) {
   var openWebApp=JSON.parse(string);
   var chromeWebApp={};
 
@@ -13,14 +13,39 @@ function convertWebApp(string) {
       }
     }
   }
-  chromeWebApp.name=openWebApp.name;
+  chromeWebApp.name=openWebApp.name; 			//required by both
+  //validate chrome version					//required by chrome, no limit owa
+  chromeWebApp.version="";
+  if (openWebApp.version) {
+    var ints=openWebApp.version.split(".",4);
+    ints.forEach(function(e,i,a) {
+      e=parseInt(e);
+      if (e<0) e=0;
+      else if (e>65535) e=65535;
+
+      if (i>0) chromeWebApp.version+=".";
+      chromeWebApp.version+=e;
+    });
+  }
+  else chromeWebApp.version="0";
+  chromeWebApp.app={};					//required by chrome
+  chromeWebApp.app.launch={};				//required by chrome
+  if (openWebApp.launch_path) {
+    chromeWebApp.launch.web_url=openWebApp.launch_path;//required by chrome
+  }
+  else { chromeWebApp.launch_url=origin; }		//"origin" of manifest, owa says
+  if (openWebApp.installs_allowed_from) {			//not exactly the same....
+    chromeWebApp.app.urls=openWebApp.installs_allowed_from;
+  }
+
   chromeWebApp.description=openWebApp.description;
-  chromeWebApp.version=openWebApp.version;
-  chromeWebApp.icons={};
-  chromeWebApp.icons["128"]=openWebApp.icons["128"];
-  if (openWebApp.launch_path) { chromeWebApp.launch_url=openWebApp.launch_path; }
-  else { chromeWebApp.launch_url="/"; }
-  chromeWebApp.homepage_url=openWebApp.developer.url;
+  if (openWebApp.icons&&openWebApp.icons["128"]) {
+    chromeWebApp.icons={};
+    chromeWebApp.icons["128"]=openWebApp.icons["128"];
+  }
+  if (openWebApp.developer&&openWebApp.developer.url) {
+    chromeWebApp.homepage_url=openWebApp.developer.url;
+  }
 
   return JSON.stringify(chromeWebApp);
 }
